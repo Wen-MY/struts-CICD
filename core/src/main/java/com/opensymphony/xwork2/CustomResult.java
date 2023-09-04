8ed85c9c8ee5eb3a5d14558eee35e0c559066ae0
WongYewKang <wongsao02@gmail.com>
2023-09-04 22:05:41 +0800

    Added CustomResult java file

---
+++ core/src/main/java/com/opensymphony/xwork2/CustomResult.java
@@ -0,0 +1,28 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.opensymphony.xwork2;
+
+/**
+ * This class is used to define custom result types for Apache Struts actions.
+ * Custom result types determine how the action's result is rendered or processed,
+ * providing flexibility to handle unique rendering or processing requirements specific to the application.
+ */
+public class CustomResult {
+    // Custom result type implementation goes here
+}
